61821c38cc04a2ce2fc85595f4a679c6538d2901
Boris Marinov <boris.marinov@tick42.com>
2015-07-07 18:31:51 +0300

    Resolve correct versions for dependencies

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function versionify(file, options) {
     var filter = options.filter,
         placeholder = options.placeholder || '__VERSION__',
         version = options.version ||
-            require(path.join(findRoot(process.cwd()), 'package.json')).version,
+            require(path.join(findRoot(file), 'package.json')).version,
         re = new RegExp(placeholder, 'g');
 
     if (filter && !filter.test(file)) {
